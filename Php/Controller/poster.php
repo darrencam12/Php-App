bc313d4ff988bc6b559af1149c83e40f6de23594
darrencam12 <darren.camilleri.a100532@mcast.edu.mt>
2018-01-11 23:36:58 +0100

    minor changes

--- Php/Controller/poster.php
+++ Php/Controller/poster.php
@@ -15,39 +15,15 @@ include('upload.php');
            <div id="postercontainer">
              <img src="Images\600x400.png" id="PosterImage"alt="img">
            </div>
-         </div>
-         <div class="col-lg-3">
-         </div>
-       </div>
-       <div class="row">
-         <div class="col-lg-3">
-         </div>
-         <div class="col-lg-3">
+
              <form action="" enctype="multipart/form-data" method="post">
                  <input id="file" name="file" type="file" />
                  <input id="Submit" name="submit" type="submit" value="Submit" />
-             </form>
-         </div>
-         <div class="col-lg-1">
-         </div>
-         <div class="col-lg-3">
-           <div class="editbutton">
-             <a href="PosterOrders.php"class="btn btn-default" id="posterbtn2"><strong>Show Sent/Pending</strong></a>
-           </div>
-         </div>
-         <div class="col-lg-2">
-         </div>
-       </div>
 
-       <div class="row">
-         <div class="col-lg-4">
-         </div>
-         <div class="col-lg-5">
-          
-         </div>
-         <div class="col-lg-3">
-         </div>
-       </div>
+             </form>
+             <a href="PosterOrders.php"class="btn btn-default" id="posterbtn2"><strong>Show Sent/Pending</strong></a>
+
+
 
 
 
