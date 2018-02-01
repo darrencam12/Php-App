89e84806a8bc89156c1056c21928be91de8d5494
darrencam12 <darren.camilleri.a100532@mcast.edu.mt>
2018-02-01 21:06:01 +0100

    added the database

--- Php/Database/db_fish_species.sql
+++ Php/Database/db_fish_species.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 13, 2018 at 07:41 PM
+-- Generation Time: Feb 01, 2018 at 09:05 PM
 -- Server version: 10.1.29-MariaDB
 -- PHP Version: 7.1.12
 
@@ -45,7 +45,34 @@ INSERT INTO `tbl_fish` (`id`, `fsh_FishName`, `fsh_ScientificName`, `fsh_Origin`
 (1, 'Red coat/red squirrelfish', 'Sargocentron rubrum', 'Indian Ocean, Pacific Ocean', 0, 0),
 (2, 'Longjaw squirrelfish', 'Holocentrus adscensionis', 'tropical western and eastern Atlantic Ocean', 0, 0),
 (3, 'Malabar grouper', 'Ephinephelus malabaricus', 'Indian Ocean, Pacific Ocean, Red Sea', 0, 0),
-(5, 'Blue-spotted seabass', 'Cephalopholis taeniops', 'tropical eastern Atlantic Ocean', 0, 0);
+(4, 'Blue-spotted seabass', 'Cephalopholis taeniops', 'tropical eastern Atlantic Ocean', 0, 0),
+(5, 'Yellowfin hind', 'Cephalopholis hemistiktos', 'Indian Ocean, Red Sea ', 0, 0),
+(6, 'African sergeant', 'Abudefuduf hoefleri', 'tropical eastern Atlantic Ocean', 0, 0),
+(7, 'Sergeant major', 'Abudefduf saxatilis', 'tropical western and eastern Atlantic Ocean', 0, 0),
+(8, 'Indo-Pacific sergeant', 'Abudefduf vaigiensis', 'Indian Ocean, Pacific Ocean, Red Sea', 0, 0),
+(9, 'Cocoa damselfish', 'Stegastes variabilis', 'tropical western Atlantic Ocean', 0, 0),
+(10, 'Azure damselfish', 'Chrysiptera hemicyanea', 'Pacific Ocean', 0, 0),
+(11, 'Doctorfish', 'Acanthurus chirurgus', 'western and eastern tropical Atlantic Ocean', 0, 1),
+(12, 'Monrovian surgeonfish', 'Acanthurus monroviae', 'tropical eastern Atlantic Ocean', 0, 0),
+(13, 'Blue tang surgeonfish', 'Acanthurus coeruleus', 'tropical western Atlantic Ocean', 0, 0),
+(14, 'Yellowbar angelfish', 'Pomacanthus maculosus', 'Indian Ocean, Red Sea ', 0, 0),
+(15, 'Guinea angelfish', 'Holocanthus africanus', 'tropical eastern Atlantic Ocean', 0, 0),
+(16, 'Red Sea bannerfish', 'Heniochus intermedius', 'Indian Ocean, Red Sea ', 0, 0),
+(17, 'African moonfish', 'Selene dorsalis', 'tropical eastern Atlantic Ocean', 0, 0),
+(18, 'Atlantic tripletail', 'Lobotes surinamensis', 'Tropical and sub-tropical waters of all the oceans', 0, 0),
+(19, 'Lesser amberjack', 'Seriola fasciata', 'Western Atlantic Ocean', 0, 0),
+(20, 'Shrimp scad', 'Alepes djebaba', 'Indian Ocean, Pacific Ocean, Red Sea', 0, 0),
+(21, 'Yellowstripe barracuda ', 'Sphyraena chrysotaenia', 'Indian Ocean, Pacific Ocean, Red Sea', 0, 0),
+(22, 'Reticulated leatherjack', 'Stephanolepis diaspros', 'Indian Ocean, Red Sea', 0, 0),
+(23, 'Barred knifejaw', 'Oplegnathus fasciatus', 'north-western Pacific Ocean', 0, 0),
+(24, 'Dory snapper', 'Lutjanus fulviflamma', 'Indian Ocean, Pacific Ocean, Red Sea', 0, 0),
+(25, 'Deep-sea rockfish', 'Pontinus kuhlii', 'eastern Atlantic Ocean', 0, 0),
+(26, 'Dusky spinefoot', 'Siganus luridus', 'Indian Ocean, Red Sea', 0, 1),
+(27, 'Silver-cheeked toadfish', 'Lagocephalus sceleratus', 'Indian Ocean, Pacific Ocean', 1, 0),
+(28, 'Blue cornet fish', 'Fistularia commersonii', 'Indian Ocean, Pacific Ocean, Red Sea', 0, 0),
+(29, 'Spotted scat', 'Scatophagus argus', 'Indian Ocean, Pacific Ocean', 0, 0),
+(30, 'Lowfin chub', 'Kyphosus vaigiensis', 'Indian Ocean, Pacific Ocean, Red Sea', 0, 0),
+(31, ' Porâ€™s goatfish', 'Upeneus pori', 'Indian Ocean, Red Sea', 0, 0);
 
 -- --------------------------------------------------------
 
@@ -61,15 +88,6 @@ CREATE TABLE `tbl_poster` (
   `ptr_Date` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `tbl_poster`
---
-
-INSERT INTO `tbl_poster` (`idPoster`, `ptr_Name`, `ptr_Surname`, `ptr_Address`, `ptr_Date`) VALUES
-(1, 'Darren', 'Camilleri', '93,romar triq lizbark tat- torok', 20180117),
-(2, 'Darren', 'Camilleri', 'tirq il mandra', 1992802),
-(3, 'Darren', 'Darren', 'tirq il mandra', 1992802);
-
 -- --------------------------------------------------------
 
 --
@@ -82,23 +100,13 @@ CREATE TABLE `tbl_report` (
   `rpt_Surname` varchar(70) NOT NULL,
   `rpt_Email` varchar(100) NOT NULL,
   `rpt_PhoneNo` int(11) NOT NULL,
+  `rpt_descrpition` varchar(2000) NOT NULL,
   `rpt_Date` int(11) NOT NULL,
   `rpt_Location` varchar(100) NOT NULL,
   `rpt_Preserved` tinyint(4) NOT NULL,
   `rpt_Donate` tinyint(4) NOT NULL,
   `rpt_Checked` tinyint(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `tbl_report`
---
-
-INSERT INTO `tbl_report` (`ID`, `rpt_Name`, `rpt_Surname`, `rpt_Email`, `rpt_PhoneNo`, `rpt_Date`, `rpt_Location`, `rpt_Preserved`, `rpt_Donate`, `rpt_Checked`) VALUES
-(1, 'Joe', 'Camilleri', 'joecam@gmail.com', 99652994, 20180117, 'Birzebbuigia', 1, 1, 0),
-(2, 'Daniel', 'Cassar', 'Cas1@gmail.com', 99874224, 20180117, 'Birzebbuigia', 0, 1, 0),
-(3, 'Robert', 'Grech', 'Rgrech@gmail.com', 99852312, 20180117, 'Birzebbuigia', 0, 0, 0),
-(4, 'Merry', 'Psalia', 'MP@gmail.com', 99852224, 20180117, 'Birzebbuigia', 1, 1, 0),
-(5, 'Darren', 'bigila', 'db@gmail.com', 99421452, 20180117, 'Birzebbuigia', 0, 1, 0);
 
 -- --------------------------------------------------------
 
@@ -117,7 +125,7 @@ CREATE TABLE `tbl_users` (
 --
 
 INSERT INTO `tbl_users` (`id`, `user_name`, `user_password`) VALUES
-(1, 'admin', '$2y$10$l6uUMyCmKrAzYDVkQtZ4fOgvf698R2SYRXQjg7W4Os3N6PDhnBTMK');
+(1, 'Admin', '$2y$10$II0CDt8pDNG8gbGHpp1meeWd3Z9A0yI433kFMFkUxgCD4ElxSbbLK');
 
 --
 -- Indexes for dumped tables
@@ -155,19 +163,19 @@ ALTER TABLE `tbl_users`
 -- AUTO_INCREMENT for table `tbl_fish`
 --
 ALTER TABLE `tbl_fish`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 
 --
 -- AUTO_INCREMENT for table `tbl_poster`
 --
 ALTER TABLE `tbl_poster`
-  MODIFY `idPoster` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `idPoster` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `tbl_report`
 --
 ALTER TABLE `tbl_report`
-  MODIFY `ID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `ID` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `tbl_users`
